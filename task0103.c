ef8986978fba00d3e1b11c28625ca30efa8e0001
vadimtukaev <vadimtukaev@gmail.com>
2019-09-11 16:29:18 +0400

    add title of table

--- task0103.c
+++ task0103.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
+    printf("Fahrengeit\tCelsius\n");
     for (int fahr = 0; fahr <= 300; fahr += 20)
-        printf("%3d %6.1f\n", fahr, 5.0 / 9.0 * (fahr -32));
+        printf("%10d\t%7.1f\n", fahr, 5.0 / 9.0 * (fahr - 32));
 }
