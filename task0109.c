612f3ecd25ef90f444c3c14e906579ef347e4bb1
vadimtukaev <vadimtukaev@gmail.com>
2019-09-11 19:28:27 +0400

    simple solution

--- task0109.c
+++ task0109.c
@@ -2,35 +2,9 @@
 
 int main(void)
 {
-    for (int state = 0; state >= 0;) {
-        int ch = getchar();
-        switch (state) {
-            case 0:
-                if (ch == EOF) {
-                    state = -1;
-                } else if (ch != ' ') {
-                    putchar(ch);
-                    state = 1;
-                }
-            break;
-            case 1:
-                if (ch == EOF) {
-                    state = -1;
-                } else if (ch != ' ') {
-                    putchar(ch);
-                } else {
-                    state = 2;
-                }
-            break;
-            case 2:
-                if (ch == EOF) {
-                    state = -1;
-                } else if (ch != ' ') {
-                    putchar(' ');
-                    putchar(ch);
-                    state = 1;
-                }
-            break;
+    for (int prev = ' ', curr; (curr = getchar()) != EOF; prev = curr) {
+        if (prev != ' ' || curr != ' ') {
+            putchar(curr);
         }
     }
 }
